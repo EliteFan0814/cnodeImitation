0cf81a4a8872ab18fd388fc296409a46ec5174b1
EliteFan <fan552426811@outlook.com>
2019-05-20 12:45:20 +0800

    three

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/cnodeImitation/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
